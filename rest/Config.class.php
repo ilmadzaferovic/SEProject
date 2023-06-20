10fd82c35864ea9b6af63e07930d8f09b9ba1514
ilmadzaferovic <ilma.dzaferovic@stu.ibu.edu.ba>
2023-06-20 18:32:01 +0200

    deploying backend

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,31 +1,33 @@
 <?php
 
 class Config {
+  public static function DB_HOST() {
+    return Config::get_env("DB_HOST", "127.0.0.1");
+  }
 
-  public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "localhost");
+  public static function DB_USERNAME() {
+    return Config::get_env("DB_USERNAME", "ilma_dzaf123");
   }
-  public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "root");
+
+  public static function DB_PASSWORD() {
+    return Config::get_env("DB_PASSWORD", "ilmailmailma");
   }
-  public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "mysqlmysqlmysql123");
+
+  public static function DB_SCHEME() {
+    return Config::get_env("DB_SCHEME", "reading_journal");
   }
-  public static function DB_SCHEME(){
-    return Config::get_env("DB_SCHEME", "rj");
-  }
-  public static function DB_PORT(){
+
+  public static function DB_PORT() {
     return Config::get_env("DB_PORT", "3306");
   }
-  public static function JWT_SECRET(){
+
+  public static function JWT_SECRET() {
     return Config::get_env("JWT_SECRET", "qfA0H7waHc");
   }
 
-  
-
-  public static function get_env($name, $default){
-   return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+  public static function get_env($name, $default) {
+    return isset($_ENV[$name]) && trim($_ENV[$name]) !== '' ? $_ENV[$name] : $default;
   }
 }
 
-?>
+
